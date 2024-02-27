02a248fc92476a53a24ee0859ee5892668ef7a67
Gustavo Figueiredo <gustavoribeiro665@hotmail.com>
2024-02-27 17:27:13 -0300

    testes da entidade checkout

--- src/checkouts/entities/checkout.entity.spec.ts
+++ src/checkouts/entities/checkout.entity.spec.ts
@@ -1,4 +1,4 @@
-import { Checkout, CreateCheckoutCommand, CreateCheckoutItem } from "./checkout.entity"
+import { Checkout, CheckoutStatus, CreateCheckoutCommand, CreateCheckoutItem } from "./checkout.entity"
 
 describe('Checkout entity test', () => {
   let checkoutInput: CreateCheckoutCommand;
@@ -31,9 +31,73 @@ describe('Checkout entity test', () => {
     }
   });
 
+  it('should be defined', () => {
+    const checkout = Checkout.create(checkoutInput);
+
+    expect(checkout).toBeDefined();
+    expect(checkout).toBeInstanceOf(Checkout);
+    expect(checkout.items.length).toBe(2);
+  });
+
   it('should calculate the total correctly', () => {
     const checkout = Checkout.create(checkoutInput);
 
     expect(checkout.total).toBe(30);
+  })
+
+  describe('pay checkout test', () => {
+    it('should be able to pay the checkout', () => {
+      const checkout = Checkout.create(checkoutInput);
+
+      checkout.pay();
+
+      expect(checkout.status).toBe(CheckoutStatus.PAID);
+    });
+
+    it('should throw when trying to pay failed checkout', () => {
+      const checkout = Checkout.create(checkoutInput);
+      checkout.status = CheckoutStatus.FAILED;
+
+      expect(() => {
+        checkout.pay();
+      }).toThrow('Checkout failed');
+    });
+
+    it('should throw when trying to pay paid checkout', () => {
+      const checkout = Checkout.create(checkoutInput);
+      checkout.status = CheckoutStatus.PAID;
+
+      expect(() => {
+        checkout.pay();
+      }).toThrow('Checkout already paid');
+    });
+  });
+
+  describe('fail checkout test', () => {
+    it('should be able to fail the checkout', () => {
+      const checkout = Checkout.create(checkoutInput);
+
+      checkout.fail();
+
+      expect(checkout.status).toBe(CheckoutStatus.FAILED);
+    });
+
+    it('should throw when trying to fail failed checkout', () => {
+      const checkout = Checkout.create(checkoutInput);
+      checkout.status = CheckoutStatus.FAILED;
+
+      expect(() => {
+        checkout.pay();
+      }).toThrow('Checkout failed');
+    });
+
+    it('should throw when trying to pay paid checkout', () => {
+      const checkout = Checkout.create(checkoutInput);
+      checkout.status = CheckoutStatus.PAID;
+
+      expect(() => {
+        checkout.pay();
+      }).toThrow('Checkout already paid');
+    });
   })
 })
