d4651ca8c6c436cea48b5fc1be491587b0ba70e3
Gustavo Figueiredo <gustavoribeiro665@hotmail.com>
2024-02-27 13:56:04 -0300

    criando resource de checkout

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CheckoutsModule } from './checkouts/checkouts.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Checkout } from './checkouts/entities/checkout.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      username: 'root',
+      password: 'root',
+      database: 'nest',
+      entities: [Checkout],
+      synchronize: true,
+      logging: true
+    }),
+    CheckoutsModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
