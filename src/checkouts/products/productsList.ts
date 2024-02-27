b8533372ef1e599c4a2dbda1c7942acdd65c73e6
Gustavo Figueiredo <gustavoribeiro665@hotmail.com>
2024-02-27 16:24:17 -0300

    produtos fakes, simulando produtos existentes no banco de dados de outro microsserviço

---
+++ src/checkouts/products/productsList.ts
@@ -0,0 +1,50 @@
+export const externalFakeProducts = [
+  {
+    "id": 1,
+    "name": "teclado mecanico",
+    "description": "teclado que escreve sozinho",
+    "image_url": "s3.algumaUrl",
+    "price": 500,
+    "category_id": 1
+  },
+  {
+    "id": 2,
+    "name": "mouse",
+    "description": "mouse que clica sozinho",
+    "image_url": "s3.algumaUrl",
+    "price": 300,
+    "category_id": 1
+  },
+  {
+    "id": 3,
+    "name": "headset",
+    "description": "som muito bom",
+    "image_url": "s3.algumaUrl",
+    "price": 400,
+    "category_id": 1
+  },
+  {
+    "id": 4,
+    "name": "ssd",
+    "description": "o mais rápido",
+    "image_url": "s3.algumaUrl",
+    "price": 500,
+    "category_id": 2
+  },
+  {
+    "id": 5,
+    "name": "memoria",
+    "description": "frequencias elevadas",
+    "image_url": "s3.algumaUrl",
+    "price": 350,
+    "category_id": 2
+  },
+  {
+    "id": 6,
+    "name": "placa de video",
+    "description": "roda crysis",
+    "image_url": "s3.algumaUrl",
+    "price": 4000,
+    "category_id": 2
+  }
+];
