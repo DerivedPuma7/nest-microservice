efaaee98e1181830b8e56cb9cb272d1f3cfe4890
Gustavo Figueiredo <gustavoribeiro665@hotmail.com>
2024-02-27 14:30:28 -0300

    adiciona relacionamentos

--- src/checkouts/entities/checkout.entity.ts
+++ src/checkouts/entities/checkout.entity.ts
@@ -1,4 +1,4 @@
-import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { Column, CreateDateColumn, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
 
 export enum CheckoutStatus {
   PENDING = 'PENDING',
@@ -19,4 +19,40 @@ export class Checkout {
 
   @CreateDateColumn()
   created_at: Date;
+}
+
+@Entity()
+export class CheckoutProduct {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: number;
+
+  @Column()
+  description: string;
+
+  @Column()
+  image_url: string;
+
+  @Column()
+  product_id: number // id do produto em outro microsserviço
+}
+
+@Entity()
+export class CheckoutItem {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  quantity: number;
+
+  @Column()
+  price: number;
+
+  @ManyToOne(() => Checkout)
+  checkout: Checkout;
+
+  @ManyToOne(() => CheckoutProduct)
+  product: CheckoutProduct;
 }
