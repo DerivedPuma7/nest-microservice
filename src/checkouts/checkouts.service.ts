7da02f0c7749b36241cf010c097713480e911f7e
Gustavo Figueiredo <gustavoribeiro665@hotmail.com>
2024-02-27 16:23:16 -0300

    implementa save do checkout

--- src/checkouts/checkouts.service.ts
+++ src/checkouts/checkouts.service.ts
@@ -1,11 +1,47 @@
 import { Injectable } from '@nestjs/common';
 import { CreateCheckoutDto } from './dto/create-checkout.dto';
 import { UpdateCheckoutDto } from './dto/update-checkout.dto';
+import { externalFakeProducts } from './products/productsList';
+import { Checkout } from './entities/checkout.entity';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
 export class CheckoutsService {
-  create(createCheckoutDto: CreateCheckoutDto) {
-    return 'This action adds a new checkout';
+  externalProducts = externalFakeProducts;
+
+  constructor(
+    /* 
+      essa implementação não é legal
+      é interessante que o tipo seja uma interface que abstraia o repositorio. dessa maneira, conseguimos 
+      ter diferentes implementações, para orms diferentes, que se encaixam na nossa aplicação
+    */
+    @InjectRepository(Checkout) private readonly checkoutRepo: Repository<Checkout>
+  ) {}
+
+  async create(createCheckoutDto: CreateCheckoutDto) {
+    const productIds: number[] = createCheckoutDto.items.map((item) => (item.product_id));
+    // const productsToSave = ... => chamada externa ao microsservico de produtos para obter informações sobre os produtos recebidos na request
+    const productsToSave = this.externalProducts.filter((product) => productIds.includes(product.id));
+
+    const checkout = Checkout.create({
+      items: createCheckoutDto.items.map(item => {
+        const product = productsToSave.find((product) => product.id === item.product_id);
+        return {
+          quantity: item.quantity,
+          price: product.price,
+          product: {
+            name: product.name,
+            description: product.description,
+            image_url: product.image_url,
+            product_id: product.id
+          }
+        }
+      })
+    });
+
+    await this.checkoutRepo.save(checkout);
+    return checkout;
   }
 
   findAll() {
